1328df970e1755beca66d01f1ec75682124d55ca
Thiago Henrique Ferreira da Silva <thiago.henriquef@hotmail.com>
2026-05-25 15:01:04 -0300

    TASK-009: define safety states

--- main/modules/common/types.h
+++ main/modules/common/types.h
@@ -9,3 +9,10 @@ typedef enum {
     APP_STATE_FAULT,
     APP_STATE_SHUTDOWN,
 } app_state_t;
+
+typedef enum {
+    SAFETY_STATE_OK = 0,
+    SAFETY_STATE_SENSOR_ERROR,
+    SAFETY_STATE_OVERTEMPERATURE,
+    SAFETY_STATE_UNDERTEMPERATURE,
+} safety_state_t;
