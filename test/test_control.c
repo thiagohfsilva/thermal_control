9182fe31262f8040bd3b302b47f58a1efccb8730
Thiago Henrique Ferreira da Silva <thiago.henriquef@hotmail.com>
2026-05-25 15:17:08 -0300

    TASK-105: test PID output saturation

--- test/test_control.c
+++ test/test_control.c
@@ -13,3 +13,19 @@ void test_pid_initializes_and_updates_output(void)
         ESP_OK, pid_update(&pid, 50.0f, 40.0f, 0.1f, &output));
     TEST_ASSERT_FLOAT_WITHIN(0.001f, 10.0f, output);
 }
+
+void test_pid_saturates_output_limits(void)
+{
+    pid_controller_t pid;
+    float output = 0.0f;
+
+    TEST_ASSERT_EQUAL(
+        ESP_OK, pid_init(&pid, 100.0f, 0.0f, 0.0f, 0.0f, 100.0f));
+    TEST_ASSERT_EQUAL(
+        ESP_OK, pid_update(&pid, 100.0f, 0.0f, 0.1f, &output));
+    TEST_ASSERT_FLOAT_WITHIN(0.001f, 100.0f, output);
+
+    TEST_ASSERT_EQUAL(
+        ESP_OK, pid_update(&pid, 0.0f, 100.0f, 0.1f, &output));
+    TEST_ASSERT_FLOAT_WITHIN(0.001f, 0.0f, output);
+}
