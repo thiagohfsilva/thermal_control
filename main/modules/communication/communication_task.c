286abfcf3d1f84c6e9199df727e4019cea2e2a00
Thiago Henrique Ferreira da Silva <thiago.henriquef@hotmail.com>
2026-05-25 15:15:30 -0300

    TASK-095: add communication task

---
+++ main/modules/communication/communication_task.c
@@ -0,0 +1,32 @@
+#include "communication.h"
+
+#include "modules/common/config.h"
+
+#include "esp_err.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include <stdbool.h>
+
+static void communication_task(void *arg)
+{
+    (void)arg;
+
+    while (true) {
+        vTaskDelay(pdMS_TO_TICKS(THERMAL_LOG_PERIOD_MS));
+    }
+}
+
+esp_err_t communication_task_start(void)
+{
+    BaseType_t result = xTaskCreate(
+        communication_task,
+        "communication",
+        COMMUNICATION_TASK_STACK_SIZE,
+        NULL,
+        COMMUNICATION_TASK_PRIORITY,
+        NULL);
+
+    return result == pdPASS ? ESP_OK : ESP_ERR_NO_MEM;
+}
