4475357706538c07226412eadfb557e38638d765
Thiago Henrique Ferreira da Silva <thiago.henriquef@hotmail.com>
2026-05-25 15:13:02 -0300

    TASK-078: implement application initialization

--- main/modules/app/app.c
+++ main/modules/app/app.c
@@ -1,6 +1,14 @@
 #include "app.h"
 
+#include "modules/app/app_state_machine.h"
+#include "modules/thermal/thermal.h"
+
 esp_err_t app_init(void)
 {
-    return ESP_OK;
+    esp_err_t ret = app_state_machine_init();
+    if (ret != ESP_OK) {
+        return ret;
+    }
+
+    return thermal_init();
 }
