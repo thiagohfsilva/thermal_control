011a00f5c404dfa591430732e8d334ed8cce96b1
Thiago Henrique Ferreira da Silva <thiago.henriquef@hotmail.com>
2026-05-25 15:02:49 -0300

    TASK-020: initialize ESP-IDF ADC driver

--- main/modules/drivers/adc_drv.c
+++ main/modules/drivers/adc_drv.c
@@ -1,10 +1,43 @@
 #include "adc_drv.h"
 
+#include "modules/common/config.h"
+
+#include "esp_adc/adc_oneshot.h"
 #include "esp_err.h"
+
+#include <stdbool.h>
+
+static adc_oneshot_unit_handle_t s_adc_handle;
+static bool s_initialized;
 
 esp_err_t adc_drv_init(void)
 {
-    return ESP_ERR_NOT_SUPPORTED;
+    if (s_initialized) {
+        return ESP_OK;
+    }
+
+    adc_oneshot_unit_init_cfg_t unit_config = {
+        .unit_id = THERMAL_NTC_ADC_UNIT,
+    };
+
+    esp_err_t ret = adc_oneshot_new_unit(&unit_config, &s_adc_handle);
+    if (ret != ESP_OK) {
+        return ret;
+    }
+
+    adc_oneshot_chan_cfg_t channel_config = {
+        .atten = THERMAL_NTC_ADC_ATTEN,
+        .bitwidth = THERMAL_NTC_ADC_BITWIDTH,
+    };
+
+    ret = adc_oneshot_config_channel(
+        s_adc_handle, THERMAL_NTC_ADC_CHANNEL, &channel_config);
+    if (ret != ESP_OK) {
+        return ret;
+    }
+
+    s_initialized = true;
+    return ESP_OK;
 }
 
 esp_err_t adc_drv_read_raw(int *raw_value)
